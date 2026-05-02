a25d9f92c8666d7ca0e17b078c2cbf29a2663af2
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-05-02 23:25:23 +0530

    fix :  fixed OOM error

--- Server/Server.js
+++ Server/Server.js
@@ -23,7 +23,6 @@ const MONGO_URI =
 const JWT_SECRET = process.env.JWT_SECRET || "secret";
 const DOC_BUILDER_URL = process.env.DOC_BUILDER_URL || "http://localhost:5002";
 
-
 // -----------------------------------------------------------------------------
 // Initialize Express app as well as middleware
 const app = express(); 
