86d23e9829325cda5b5d17e691974ab3053d736b
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-05-12 10:48:10 +0530

    fix : fixed OOM error

--- Server/Server.js
+++ Server/Server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const fs = require("fs");
+const fs = require("fs"); 
 const path = require("path");
 const axios = require("axios");
 const cors = require("cors");
